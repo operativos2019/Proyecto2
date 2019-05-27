0acb4ad96020ebb6303b6c88baa9b050350bcac8
JeanPaul <jeanpaullm@gmail.com>
2019-05-27 14:55:08 -0600

    added driver info

--- driver/checkersDriver.c
+++ driver/checkersDriver.c
@@ -2,7 +2,10 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-MODULE_LICENSE("Dual BSD/GPL");
+MODULE_LICENSE("Dual BSD/GPL");    // license type
+MODULE_AUTHOR("Grupo2");     // author
+MODULE_DESCRIPTION("Linux driver for a Checkers Player Robot.");  // description
+MODULE_VERSION("0.1");    // version of the module
 
 static int __init hello_init(void) {
     printk("<1> Hello world!\n");
